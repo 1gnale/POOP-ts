2dc9203466bc03754f3f2d15958297cc2155c892
Ignale <nosoyignacio@correo.com>
2024-07-08 09:42:26 -0300

    chore: update babel.config.cjs

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,7 +3,7 @@
 module.exports = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
+    ['@babel/preset-typescript', {isTSX: true, allExtensions: true}],
     "@babel/preset-react",
   ],
   plugins: [
