c6a69ddec0d1fd80c1ea2c452a8668a9db8c53a9
Ignale <nosoyignacio@correo.com>
2024-07-11 00:42:41 -0300

    styles: normalize cards & add styles to scrollbar

--- src/components/desktopCard/DesktopCard.tsx
+++ src/components/desktopCard/DesktopCard.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 
 interface CardProps {
-    children: React.ReactNode;
-    title: string;
+  title: string;
+  children: React.ReactNode;
 }
 
 const DesktopCard: React.FunctionComponent<CardProps> = ({ children, title }) => {
     return (
-        <div className="desktopCard">
-            <h1>{title}</h1>
+        <div className="max-w-sm rounded-xl shadow-lg h-96 overflow-auto scrollbar-custom text-center bg-slate-50">
+            <h1 className="font-bold text-xl mb-2">{title}</h1>
             {children}
         </div>
     )
