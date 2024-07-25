2cea4a7c6c9f6864e7590e5e153537c27d9b626b
Ignale <nosoyignacio@correo.com>
2024-07-25 12:59:15 -0300

    feat: add mock multi select

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -6,7 +6,7 @@ interface NavBarProps {
 
 const NavBar: React.FunctionComponent<NavBarProps> = ({ children }) => {
     return (
-        <nav className="fixed top-0 px-4 py-4 flex min-h-20 justify-between min-w-full items-center bg-white dark:bg-slate-900 z-30">
+        <nav className="fixed top-0 px-4 py-4 flex min-h-24 justify-between min-w-full items-center bg-white dark:bg-slate-900 z-30">
             {children}
         </nav>
     );
