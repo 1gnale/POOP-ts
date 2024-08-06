a772800347af9f0d428cf6008c2693f11a12e154
Ignale <nosoyignacio@correo.com>
2024-08-06 17:30:29 -0300

    refactor: add useGetAllData to store all data

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import Header from "../header/Header.tsx";
 import Footer from "../footer/Footer.tsx";
 import DesktopCardPoemContainer from "../desktopCardPoemContainer/DesktopCardPoemContainer.tsx";
 import MobileCardPoemContainer from "../mobileCardPoemContainer/MobileCardPoemContainer.tsx";
-import useGetPoems from "../customeHooks/useGetPoem.ts";
+import useGetAllData from "../customeHooks/useGetAllData.ts";
 import useScreenSize from "../customeHooks/useScreenSize.ts";
 import Loader from "../loader/Loader.tsx";
 import { Poem } from "../../types";
@@ -11,28 +11,35 @@ import { useAppSelector } from "../../redux/reduxTypedHooks.ts";
 
 const Layout: React.FunctionComponent = () => {
     const screenSize: number = useScreenSize();
-    useGetPoems();
+    const { error, loading } = useGetAllData();
     const poems: Poem[] = useAppSelector((state) => state.poems.poems);
+
+    if (loading) {
+        return (
+            <div className="">
+                <Loader />
+            </div>
+        )
+    }
+
+    if (error) {
+        return <div className="error-message">Error: something went wrong</div>;
+    }
+
     return (
         <>
             <Header />
             <div className="bg-amber-100 mt-24 dark:bg-slate-700">
                 {
                     screenSize > 960 ?
-                        poems.length === 0 ?
-                            <Loader />
-                            :
-                            <DesktopCardPoemContainer poems={poems} />
+                        <DesktopCardPoemContainer poems={poems} />
                         :
-                        poems.length === 0 ?
-                            <Loader />
-                            :
-                            <MobileCardPoemContainer poems={poems} />
+                        <MobileCardPoemContainer poems={poems} />
                 }
                 <Footer />
             </div>
         </>
     );
-}
+};
 
 export default Layout;
